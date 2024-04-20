6a5310521b33fcc89e9fa4919c7f2dc13caff858
yaonur <ya_onur@hotmailc.om>
2024-04-20 04:03:08 +0300

    ss

---
+++ go/endtoend/s1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Player interface {
+	KickBall()
+}
+
+type FootballPlayer struct {
+	stamina int
+	power   int
+}
+
+func (f FootballPlayer) KickBall() {
+	shot := f.stamina + f.power
+	fmt.Println("Kicking the ball with power: ", shot)
+}
+
+func main() {
+	team := make([]Player, 11)
+	for i := 0; i < len(team); i++ {
+		team[i] = FootballPlayer{
+			stamina: rand.Intn(10),
+			power:   rand.Intn(10),
+		}
+	}
+	for i := 0; i < len(team); i++ {
+		team[i].KickBall()
+	}
+}
