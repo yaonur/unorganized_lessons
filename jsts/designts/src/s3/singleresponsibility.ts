3238d5419c7369bbe9fb5a7a3165eed0b63062d2
yaonur <ya_onur@hotmailc.om>
2024-05-15 06:29:03 +0300

    ss

--- jsts/designts/src/s3/singleresponsibility.ts
+++ jsts/designts/src/s3/singleresponsibility.ts
@@ -1,4 +1,39 @@
 
+class BlogPost {
+	title: string;
+	content: string;
+	constructor(title: string, content:string){
+		this.title =title
+		this.content = content;
+	}
+
+	createPost() {
+	
+	}
+
+	updatePost() {
+
+	}
+
+	deletePost() {
+
+	}
+	
+	// bad approach
+	// displayHTML() {
+	// 	return `<h1>${this.title}</h1><p>${this.content}</p>`
+	// }
+}
+
+class BlogPostDisplay {
+	constructor(public blogPost:BlogPost) {
+
+	}
+	displayHTML() {
+		return `<h1>${this.blogPost.title}</h1><p>${this.blogPost.content}</p>`
+	}
+}
+
 
 export function main() {
 	console.log("single")
