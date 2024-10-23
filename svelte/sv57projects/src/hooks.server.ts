feabc3252b2b50a79b36a0f43a25daebb9b2de7e
yaonur <ya_onur@hotmail.com>
2024-10-23 22:27:00 +0300

    ss

--- svelte/sv57projects/src/hooks.server.ts
+++ svelte/sv57projects/src/hooks.server.ts
@@ -1,24 +1,28 @@
 import type { Handle } from '@sveltejs/kit';
 import { i18n } from '$lib/i18n';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-const handleParaglide: Handle = i18n.handle();
+// const handleParaglide: Handle = i18n.handle();
 // export const handle: Handle = handleParaglide;
+
 export const handle: Handle = async ({ event, resolve }) => {
 	// console.log("<-----hooks.ts----------->")
-	// console.log("hostName:",event.url.hostname)
+	// console.log("resolve:",resolve)
 	// console.log("event:",event)
+	// add a sample header to the response
 	const response = await i18n.handle()({ event, resolve });
+	response.headers.set('x-custom-header', 'hello world');
+	// console.log("response:",response)
 	const setCookie = response.headers.get('Set-Cookie');
-	console.log("Set-Cookie header:", setCookie);
-
+	// console.log("Set-Cookie header:", setCookie);
+	
 	if (response) {
 		return response;
 	}
-
-
-
+	
+	
+	
 	
 	console.log('something went wrong returning svelte response');
 	const svelteKitResponse = await resolve(event);
+	console.log("svelteKitResponse:",svelteKitResponse)
 	return svelteKitResponse;
 };
--- svelte/sv57projects/src/hooks.ts
+++ svelte/sv57projects/src/hooks.ts
@@ -1,2 +1,4 @@
 import { i18n } from '$lib/i18n';
 export const reroute = i18n.reroute();
+
+
