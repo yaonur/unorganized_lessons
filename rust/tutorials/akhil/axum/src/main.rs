743a9aae1e01cd51890daa005f3146d892055055
yaonur <ya_onur@hotmail.com>
2024-12-18 23:48:17 +0300

    ss

---
+++ rust/tutorials/akhil/axum/src/main.rs
@@ -0,0 +1,17 @@
+mod handler;
+mod model;
+mod route;
+mod schema;
+
+use std::sync::Arc;
+
+use axum::http::{
+
+};
+use sqlx::{postgres::PgPoolOptions, Pool, postgres};
+#[tokio::main]
+async fn main() {
+    let pi = 3.14;
+    println!("Hello, world!");
+    let i = 22;
+}
