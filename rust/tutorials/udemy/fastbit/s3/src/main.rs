e891be389a1833b5d5552f70616146939ca69d44
yaonur <ya_onur@hotmail.com>
2025-03-01 02:11:17 +0300

    ss

--- rust/tutorials/udemy/fastbit/s3/src/main.rs
+++ rust/tutorials/udemy/fastbit/s3/src/main.rs
@@ -4,4 +4,7 @@ fn main() {
     let code2 = 43 as char;
     println!("code = {}", code as char);
     println!("code2 = {}", code2);
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("arr = {:?}", arr);
+
 }
